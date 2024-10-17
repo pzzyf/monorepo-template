9abf27c149a3f54468de56a072fd98fa16c4c4eb
pzzyf <2279948211@qq.com>
2024-10-17 23:53:36 +0800

    feat: vite env config

--- internal/vite-config/src/config/application.ts
+++ internal/vite-config/src/config/application.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { loadAndConvertEnv } from '../utils/env'
 
 function defineApplicationConfig(userConfigPromise?: any) {
-  return defineConfig(async () => {
+  return defineConfig(async (config) => {
 
-    const option = await userConfigPromise
+    const option = await userConfigPromise?.(config)
+
+    const test = await loadAndConvertEnv()
 
     console.log(option);
+
+    console.log(test);
+
 
     return {
       plugins: [vue()]
