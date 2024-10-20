3444c8a420481fb03e8f1c0fe772f4dd32e5f966
pzzyf <2279948211@qq.com>
2024-10-20 21:55:43 +0800

    vite config除了css和plugin

--- internal/vite-config/src/config/application.ts
+++ internal/vite-config/src/config/application.ts
@@ -3,18 +3,38 @@ import vue from '@vitejs/plugin-vue'
 import { loadAndConvertEnv } from '../utils/env'
 import { getCommonConfig } from './common'
 import { mergeConfig } from 'vite'
+import type { UserConfig } from 'vite'
 
 function defineApplicationConfig(userConfigPromise?: any) {
   return defineConfig(async (config) => {
 
     const options = await userConfigPromise?.(config)
 
-    const { port } = await loadAndConvertEnv()
+    const { base, port } = await loadAndConvertEnv()
 
     const { vite = {} } = options || {};
 
+    const { command } = config
 
-    const applicationConfig = {
+    const isBuild = command === 'build';
+
+
+    const applicationConfig: UserConfig = {
+      base,
+      build: {
+        rollupOptions: {
+          output: {
+            assetFileNames: '[ext]/[name]-[hash].[ext]',
+            chunkFileNames: 'js/[name]-[hash].mjs',
+            entryFileNames: 'jse/index-[name]-[hash].mjs',
+          },
+        },
+        target: 'es2015',
+      },
+      esbuild: {
+        drop: isBuild ? ['debugger'] : [],
+        legalComments: 'none',
+      },
       plugins: [vue()],
       server: {
         host: true,
