e0b9ea4684459d1bb7df9a59f812f34fffd2aaa4
pzzyf <2279948211@qq.com>
2025-05-06 17:23:08 +0800

    nprogress

--- apps/fe/src/bootstrap.ts
+++ apps/fe/src/bootstrap.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import './style.css'
+import './nprogress.css'
 
 function bootStrap() {
   const app = createApp(App)
