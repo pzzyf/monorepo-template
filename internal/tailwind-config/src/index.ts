2993d4a3126204d09d0b753db06d33866e59d8bf
pzzyf <2279948211@qq.com>
2025-05-21 09:56:33 +0800

    tw

--- internal/tailwind-config/src/index.ts
+++ internal/tailwind-config/src/index.ts
@@ -30,9 +30,15 @@ export default {
     ),
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        float: `0 6px 16px 0 rgb(0 0 0 / 8%),
+          0 3px 6px -4px rgb(0 0 0 / 12%),
+          0 9px 28px 8px rgb(0 0 0 / 5%)`,
+      },
+      colors: { ...shadcnUiColors },
+    },
   },
-  colors: { ...shadcnUiColors },
   plugins: [],
 }
 
